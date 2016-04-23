284eb2f0d11d2a4829f72358b4af5b4d682d4c32
Ben <benboyce757@outlook.com>
2016-04-23 13:47:37 -0400

    Fixed an issue with default hud showing

--- simplicityhud/cl_hud.lua
+++ simplicityhud/cl_hud.lua
@@ -5,7 +5,7 @@
 ////////////////////////////////////////////////////////////////
 
 local hideDarkRPElements = {
-  ["DarkRP_LocalPlayerHUD"] = false,
+  ["DarkRP_LocalPlayerHUD"] = true,
 }
 
 local hideDefaultHud = {
